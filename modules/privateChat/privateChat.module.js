d926f3992c4a2876084a53f3116e5722368dacde
Mattstacey321 <dhpro1998@gmail.com>
2020-05-23 16:08:03 +0700

    update dependecy, remove unused variable

--- modules/privateChat/privateChat.module.js
+++ modules/privateChat/privateChat.module.js
@@ -1,11 +1,10 @@
 const privateChatInput = require('./privateChat.input');
 const privateChatSchema = require('./privateChat.schema');
 const privateChatType = require('./privateChat.type');
-const privateChatEnum = require('./privateChat.enum');
 const privateChatResolvers = require('./privateChat.resolvers');
 module.exports = {
     PrivateChatModule: () => [
-        privateChatInput, privateChatType, privateChatSchema, privateChatEnum
+        privateChatInput, privateChatType, privateChatSchema
     ],
     PrivateChatResolvers:  privateChatResolvers
 }
